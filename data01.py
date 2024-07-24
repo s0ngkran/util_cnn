1ef39959654385f28d4cd9cf71d2874537597f7e
s0ngkran <jinnawat8@gmail.com>
2024-07-24 19:29:54 +0700

    vgg_on_plain

--- data01.py
+++ data01.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 import os
 import json
+from PIL import Image
+from torchvision import transforms 
 
 from dataclasses import dataclass
 
@@ -8,39 +10,87 @@ from dataclasses import dataclass
 class Data:
     gt: str
     img_path: str
+    
+    def __post_init__(self):
+        root = '../poh_plain_vs_natural'
+        self.img_path = os.path.join(root, self.img_path)
 
 class MyDataset(Dataset):
     def __init__(self, dataset, test_mode=False):
         assert dataset in ['tr', 'va', 'te']
-        data = self.read_data()
+        dataset = {
+            'tr': 'training',
+            'va': 'validation',
+            'te': 'testing'
+        }[dataset]
+        data = self.read_data(dataset)
 
         if test_mode:
             data = data[:100]
-            print(f'test mode {len(data)=}')
+            print(f'checking mode {len(data)=}')
             
         self.ground_truth = []
-        self.keypoint = []
+        self.img_path = []
         for i, dat in enumerate(data):
             self.ground_truth.append(dat.gt)
-            self.keypoint.append(dat.keypoints)
+            self.img_path.append(dat.img_path)
         print(f'loaded {len(self.ground_truth)=}')
 
     def __getitem__(self, idx):
+        path = self.img_path[idx]
+        img = self.load_img(path)
+        gt  =self.ground_truth[idx]
         ans = {
-            'inp': self.keypoint[idx],
-            'ground_truth': self.ground_truth[idx],
+            'inp': img,
+            'ground_truth': gt,
         }
         return ans
 
-    def read_data(self, mode):
-        root = '../data_zip/TFS_single_hand'
-        path = os.path.join(root, 'gt.json')
+    def load_img(self, image_path, log=False):
+        image = Image.open(image_path)
+        # 0-255
+        if log:
+            print()
+            print('-----log load img()')
+            print('bef image size|mode =', image.size, image.mode)
+
+        img_size = 220
+        preprocess = transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.RandomRotation(5),        
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),  
+            transforms.RandomResizedCrop(img_size, scale=(0.75, 1), ratio=(1.0, 1.0)), 
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                              std=[0.229, 0.224, 0.225]),
+        ])
+        image_tensor = preprocess(image)
+        if log:
+            print('aft image size|min|max =', image_tensor.size(), image_tensor.min(), image_tensor.max())
+            # approx -> -2 to 2
+            image_array = image_tensor.numpy()
+            print('min, max =',image_array.min(), image_array.max())
+            print('-----end log load img()')
+            print()
+        
+        return image_tensor
+
+    def read_data(self, dataset):
+        root = '../poh_plain_vs_natural/processed'
+        path = os.path.join(root, f'poh_black_{dataset}.json')
         with open(path, 'r') as f:
             data = json.load(f)
-        
         out = []
-        print(f'{len(out)=} {len(no_kp)=}')
+        for d in data:
+            d = Data(d['gt'], d['img_path'])
+            out.append(d)
+        print('example data:')
+        print(d)
+        print()
+        print(f'{dataset}_set {len(out)=}')
         return out
+
     def read_json(self, path):
         path = os.path.join(path)
         with open(path, 'r') as f:
@@ -51,11 +101,10 @@ class MyDataset(Dataset):
         return len(self.ground_truth)
 
 def test():
-    # data = MyDataset('tr', 'abs')
-    # print()
-    # for d in data:
-    #     print(d)
-    #     break
+    data = MyDataset('tr')
+    for d in data:
+        print(d['inp'].shape, d['ground_truth'])
+        break
     pass
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,91 @@
+import torch.nn as nn
+import torchvision.models as models
+import os
+import torch
+
+class MyModel(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        model = models.vgg16(pretrained=True)
+        model = nn.Sequential(*list(model.children())[:-1])
+        for name, param in model.named_parameters():
+            param.requires_grad = False
+
+        self.vgg16 = model # last layer shape -> 512 x 7 x 7
+
+        n_hidden1 = 2048
+        n_hidden2 = 2048
+        n_out = 30
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, n_hidden1),
+            nn.BatchNorm1d(n_hidden1),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(n_hidden1, n_hidden2),
+            nn.BatchNorm1d(n_hidden2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(n_hidden2, n_out),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.vgg16(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def test_forword():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device, 'device')
+    os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+    model = MyModel()
+    model = model.to(device)
+    inp_size = 360
+    # input_tensor = torch.rand(5, 3, 64, 64).to(device)
+    input_tensor = torch.rand(1, 3, inp_size, inp_size).to(device)
+    print(input_tensor.shape, 'input tensor')
+    output = model(input_tensor)
+    if type(output) == tuple:
+        for out in output:
+            print('out shape',out.shape)
+    else:
+        print('out sh',output.shape)
+        print('output',output)
+    d = {}
+    argmax = torch.argmax(output, 1)
+    print(argmax.tolist(), '---')
+    a_json = argmax.tolist()[0]
+    d['d1'] = str(a_json)
+    import json
+    with open('test_save.json', 'w') as f:
+        json.dump(d, f)
+    with open('test_save.json', 'r') as f:
+        dat = json.load(f)
+        print('---', dat)
+        
+def test_with_loader():
+    from data01 import MyDataset
+    from torch.utils.data import DataLoader
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # force cpu
+    # device = torch.device('cpu')
+    print(device, 'device')
+
+    dataset = MyDataset('tr', test_mode=True)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    model = MyModel()
+    model = model.to(device)
+    for i, dat in enumerate(dataloader):
+        img = dat['inp'].to(device)
+        print(img.shape, 'inp shape from loader')
+        output = model(img)
+        print(output.shape, 'out shape from model')
+        break
+        
+if __name__ == '__main__':
+    # test_forword()
+    test_with_loader()
+        
