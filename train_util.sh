a05fb9f6873b3e0507116adb07040ea1df723616
s0ngkran <jinnawat8@gmail.com>
2024-08-11 01:51:44 +0700

    uptrainutil

--- train_util.sh
+++ train_util.sh
@@ -1,15 +1,25 @@
 
-TZ='Asia/Bangkok'; export TZ;
+TZ='Asia/Bangkok'; export TZ; 
 date;
+
+dateLocal(){
+    is_plus_7=$1
+    if [ -z "$is_plus_7" ]; then
+        date;
+    else
+        date -d "+7 hours"; 
+    fi
+}
 
 wait_gpu() {
     gb=$1
     gb=$(expr $gb '*' 1024) 
+    localTime=$2
     # 20480 == 20GB
     threshold=$gb
     while true; do
         mem=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits | awk '{print $1}' | sort -n | tail -1)
-        echo "GPU= $mem MB; wait --> $gb MB|$(date) Thailand";
+        echo "GPU= $mem MB; wait --> $gb MB|$(dateLocal localTime) Thailand";
         if (( mem > threshold )); then
             break
         fi
