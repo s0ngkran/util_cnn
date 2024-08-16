ddaa02e2a119d4d604a1ea4d01acb63843f64b4c
s0ngkran <jinnawat8@gmail.com>
2024-08-16 15:25:21 +0700

    acc

--- train_mozart.sh
+++ train_mozart.sh
@@ -3,8 +3,30 @@ echo "train..."
 source $(dirname $0)/train_util.sh
 echo 'after source';
 
+
+# i=0
+# tr s360 BMlr1_${i} "-col -lr -1 -s 70";
+# echo sleep300
+# sleep 300;
+# wait_gpu 10;
+# i=1
+# tr s360 BMlr1_${i} "-col -lr -1 -s 70";
+# echo sleep300
+# sleep 300;
+
+# for i in {2..6}
+# do
+#     wait_gpu 10 
+#     tr s360 BMlr1_${i} "-lr -1 -s 70"
+#     echo sleep300
+#     sleep 300;
+# done
+
+
 for i in {0..4}
 do
-    wait_gpu 10 true
-    tr 4x _Mlr1_${i} "-lr -1 -s 70"
+    wait_gpu 2 
+    tr s64 BMlr1_${i} "-lr -1 -s 70"
+    echo sleep300
+    sleep 300;
 done
