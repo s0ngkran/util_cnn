1c20b22704b69937c01ff6f01749b77cb5dd6036
s0ngkran <jinnawat8@gmail.com>
2024-08-26 00:36:33 +0700

    acc 360

--- train_mozart.sh
+++ train_mozart.sh
@@ -22,11 +22,18 @@ echo 'after source';
 #     sleep 300;
 # done
 
+act(){
+    i=$1
+    tr s360 BMlr1_${i} "-lr -1 -s 500 -col";
+    sleep 100;
+    te s360_BMlr1_${i} "360 -d cuda";
+}
 
-for i in {0..4}
+
+for i in 0 2
 do
-    wait_gpu 2 
-    tr s64 BMlr1_${i} "-lr -1 -s 70"
+    wait_gpu 6 
+    act $i
     echo sleep300
     sleep 300;
 done
