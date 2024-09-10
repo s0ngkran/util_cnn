c6e47717e983a6936757a352dd5280f49339241b
s0ngkran <jinnawat8@gmail.com>
2024-09-10 21:53:02 +0700

    config y

--- config.py
+++ config.py
@@ -63,10 +63,51 @@ def config():
             'sigma_links': [ref *6, ref*6, ref*6],
             'img_size': 720,
         },
+        '8x': {
+            'sigma_points': [ref *8, ref*8, ref*8],
+            'sigma_links': [ref *8, ref*8, ref*8],
+            'img_size': 720,
+        },
+        '9x': {
+            'sigma_points': [ref *9, ref*9, ref*9],
+            'sigma_links': [ref *9, ref*9, ref*9],
+            'img_size': 720,
+        },
         '10x': {
             'sigma_points': [ref *10, ref*10, ref*10],
             'sigma_links': [ref *10, ref*10, ref*10],
             'img_size': 720,
+        },
+        '12x': {
+            'sigma_points': [ref *12, ref*12, ref*12],
+            'sigma_links': [ref *12, ref*12, ref*12],
+            'img_size': 720,
+
+        },
+        '1y': {
+            'sigma_points': [ref *1, ref*1, ref*1],
+            'sigma_links': [ref *1, ref*1, ref*1],
+            'img_size': 360,
+        },
+        '4y': {
+            'sigma_points': [ref *4, ref*4, ref*4],
+            'sigma_links': [ref *4, ref*4, ref*4],
+            'img_size': 360,
+        },
+        '7y': {
+            'sigma_points': [ref *7, ref*7, ref*7],
+            'sigma_links': [ref *7, ref*7, ref*7],
+            'img_size': 360,
+        },
+        '10y': {
+            'sigma_points': [ref *10, ref*10, ref*10],
+            'sigma_links': [ref *10, ref*10, ref*10],
+            'img_size': 360,
+        },
+        '13y': {
+            'sigma_points': [ref *13, ref*13, ref*13],
+            'sigma_links': [ref *13, ref*13, ref*13],
+            'img_size': 360,
         },
 
         # curriculum training -> acc
